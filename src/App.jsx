c76ca6a6d418aa8fa58482079f3513122c415cc1
MisterB <npghsdigitech@gmail.com>
2025-05-07 16:58:42 +1200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   const handleSpotifyLogin = () => {
     const clientId = 'bd96946fb9404e2f832ad9e12cc0096c';
-    const redirectUri = 'https://localhost:5173/callback';
+    const redirectUri = window.location.hostname === 'localhost' ? 'https://localhost:5173/callback' : 'https://misterrgb.github.io/spottystats/';
     const scopes = [
       'user-read-private',
       'user-read-email',
